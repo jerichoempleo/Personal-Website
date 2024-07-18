c9be610bdaa48997e1cc7a1e82674533c6ebbb3c
jerichoempleo <92818604+jerichoempleo@users.noreply.github.com>
2024-07-18 15:09:39 +0800

    updated gawa ni rey

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Personal-Website
+# How to run?
+ 1. Go to index.html and right click then select "Open in Default Browser"
  
--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize ScrollReveal
+  const sr = ScrollReveal({
+    distance: '100px',
+    duration: 800,
+    reset: false,
+    easing: 'ease-in-out',
+  });
+
+  // Apply ScrollReveal to sections with delay
+  sr.reveal('#profile', { origin: 'top', delay: 200 });
+  sr.reveal('#about', { origin: 'left', delay: 200 });
+  sr.reveal('.section-container', { origin: 'right', delay: 400 });
+  sr.reveal('.col', { origin: 'bottom', delay: 500 });
+  sr.reveal('.educ', { origin: 'bottom', delay: 600 });
+  sr.reveal('.work', { origin: 'bottom', delay: 700 });
+  sr.reveal('#experience', { origin: 'bottom', delay: 400 });
+  sr.reveal('.prog', { origin: 'left', delay: 400 });
+  sr.reveal('.db', { origin: 'right', delay: 400 });
+  sr.reveal('.projRow1', { origin: 'left', delay: 400 });
+  sr.reveal('.projRow2', { origin: 'right', delay: 400 });
+  sr.reveal('#contact', { origin: 'bottom', delay: 400 });
+});
+
